e930f83801d65c044d37a101e9a42e0315b25167
Carlos <65383720+FunkyRobot73@users.noreply.github.com>
2021-09-13 21:54:30 -0400

    Add files via upload

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  //console.log(`Does ${actual} = ${expected} ??`)
+  //console.log((console.assert(actual === expected)));
+  if (actual === expected ? console.log(`\uD83D\uDE00Assertion Passed: [${actual}] === [${expected}]`) : console.log(`😈Assertion Failed: [${actual}] !== [${expected}]`));
+  
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("jimmy", "jimmy");
